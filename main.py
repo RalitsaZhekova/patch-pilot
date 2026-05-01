d0de621b12cdb38fdf4839a8e4a0c2cba0ccd5e2
RalitsaZhekova <ralitsa_zhekova@abv.bg>
2026-05-01 10:52:23 +0300

    Add verbose tag for development & accept a list of content messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -11,20 +12,35 @@ def main():
 
     parser = argparse.ArgumentParser(description="PatchPilot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+    messages = [types.Content(
+        role="user",
+        parts=[types.Part(text=args.user_prompt)]
+    )]
+
     client = genai.Client(api_key=api_key)
+    response = generate_content(client, messages)
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt
+        contents=messages
     )
 
     if response is None or response.usage_metadata is None:
         raise RuntimeError("Response not set")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    return response
+
 
 if __name__ == "__main__":
     main()
